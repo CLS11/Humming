ad678747fe19b362d6602f403d626edecb0467af
crystall.101600 <crystall.101600@gmail.com>
2025-06-05 09:20:56 +0000

    Added the authentication to display either the home page if already logged in or onboarding screens.

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:humming/auth/login_or_register.dart';
+import 'package:humming/pages/home_page.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
   }
 }
