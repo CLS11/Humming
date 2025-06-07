e2d29b963dcdc437939461cc0bfb25daae826abd
crystall.101600 <crystall.101600@gmail.com>
2025-06-07 09:11:43 +0000

    Removed const from the home page.

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:humming/services/auth/login_or_register.dart';
 import 'package:humming/pages/home_page.dart';
+import 'package:humming/services/auth/login_or_register.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -13,7 +13,7 @@ class AuthGate extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomePage();
+            return HomePage();
           } else {
             return const LoginOrRegister();
           }
