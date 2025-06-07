3330a620515cccd54049bd67a11a87843a9be540
crystall.101600 <crystall.101600@gmail.com>
2025-06-07 13:05:38 +0000

    Added methods for sending and receiving messages.

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:humming/models/message.dart';
 
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   Stream<List<Map<String, dynamic>>> getUserStream() {
     return _firestore.collection('Users').snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
@@ -9,5 +13,43 @@ class ChatService {
         return user;
       }).toList();
     });
+  }
+
+  //Sending messages
+  Future<void> sendMessage(String receiverID, String message) async {
+    //Fetching the user's information
+    final currentUserID = _auth.currentUser!.uid;
+    final currentUserEmail = _auth.currentUser!.email;
+    //Fetching the timestamp
+    final timeStamp = Timestamp.now();
+    //Create a new message
+    final newMessage = Message(
+      senderID: currentUserID,
+      senderEmail: currentUserEmail!,
+      receiverID: receiverID,
+      message: message,
+      timeStamp: timeStamp,
+    );
+    //Making a chatroom
+    final ids = <String>[currentUserID, receiverID]..sort();
+    final chatRoomID = ids.join('_');
+    await _firestore
+        .collection('chatrooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .add(newMessage.toMap());
+  }
+
+  //Receiving messages
+  Stream<QuerySnapshot> getMessages(String userID, String otherUserID) {
+    //Creating a chatroom
+    final ids = <String>[userID, otherUserID]..sort();
+    final chatRoomID = ids.join('_');
+    return _firestore
+        .collection('chatrooms')
+        .doc(chatRoomID)
+        .collection('messages')
+        .orderBy('timestamp', descending: false)
+        .snapshots();
   }
 }
